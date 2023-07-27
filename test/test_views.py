ba216bd041b7edd04ed50d392bd235aeaac9f543
ndvisme <herespier@yahoo.com>
2023-07-27 14:22:15 +0300

    Add flow test method for post task

--- test/test_views.py
+++ test/test_views.py
@@ -1,6 +1,26 @@
+import pytest
 from app import app
+from views import job_that_executes_once
 
-def test_hello_world():
-    response = app.test_client().get('/')
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_task_endpoint(client, mocker):
+    # Mock the behavior of schedule.every().seconds.do() to avoid scheduling the job
+    mocker.patch('schedule.every')
+
+    # Call the job_that_executes_once directly to simulate the scheduler job
+    job_that_executes_once()
+
+    # Send a POST request to '/task' with empty JSON data
+    response = client.post('/task', json={})
+
+    # Assert the response status code and content
     assert response.status_code == 200
-    assert response.data == b'Hello, World!'
+    assert response.get_json() == {"message": "Task received successfully"}
+
+    # Assert that job_that_executes_once() was called
+    schedule.every.assert_called_once_with(5)
