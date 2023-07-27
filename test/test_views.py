6f031cc38674e2684a178ada020faada344c8981
ndvisme <herespier@yahoo.com>
2023-07-27 14:35:15 +0300

    Add unit tests for post feature.

--- test/test_views.py
+++ test/test_views.py
@@ -19,8 +19,52 @@ def test_task_endpoint(client, mocker):
     response = client.post('/task', json={})
 
     # Assert the response status code and content
-    assert response.status_code == 200
+    assert response.status_code == 201
     assert response.get_json() == {"message": "Task received successfully"}
 
     # Assert that job_that_executes_once() was called
     schedule.every.assert_called_once_with(5)
+
+def test_task_valid_post(client):
+    """Test the '/task' route."""
+    # Prepare the JSON data to send with the request
+    json_data = message_data_json_dummy()
+
+    # Make a POST request to the '/task' route
+    response = client.post('/task', json=json_data)
+
+    # Check the response status code and message
+    assert response.status_code == 201
+    assert response.json['message'] == "Task received successfully"
+
+
+def test_task_route_missing_data(client):
+    """Test the '/task' route with missing data."""
+    missing_attribute = 'group_ids'
+    json_data = message_data_json_dummy()
+    del json_data[missing_attribute]
+
+    # Make a POST request to the '/task' route
+    response = client.post('/task', json=json_data)
+
+    # Check the response status code and message
+    assert response.status_code == 400
+    assert response.json['message'] == "The {missing_attribute} attribute is missing in the input to create a task."
+
+def test_task_route_accepts_post(client):
+    response = client.post('/task')
+    assert response.status_code == 201
+
+def test_hello_world():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!'
+
+
+def message_data_json_dummy() -> dict:
+    return {
+        "user_id": "342342fsd",
+        "group_ids": ["sdfds", "2131fsdf"],
+        "message_data": "test data",
+        "time_to_send": "yyyy-MM-dd"
+    }
