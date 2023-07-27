af49dbbcc845f7fa71912bd27dd4636a45a75e59
ndvisme <herespier@yahoo.com>
2023-07-27 14:42:34 +0300

    Add /task route to view.

--- test/test_views.py
+++ test/test_views.py
@@ -1,6 +1,6 @@
 import pytest
 from app import app
-from views import job_that_executes_once
+from views import job_that_executes_once, TASK_ROUTE
 
 
 @pytest.fixture
@@ -16,7 +16,7 @@ def test_task_endpoint(client, mocker):
     job_that_executes_once()
 
     # Send a POST request to '/task' with empty JSON data
-    response = client.post('/task', json={})
+    response = client.post(TASK_ROUTE, json={})
 
     # Assert the response status code and content
     assert response.status_code == 201
@@ -31,7 +31,7 @@ def test_task_valid_post(client):
     json_data = message_data_json_dummy()
 
     # Make a POST request to the '/task' route
-    response = client.post('/task', json=json_data)
+    response = client.post(TASK_ROUTE, json=json_data)
 
     # Check the response status code and message
     assert response.status_code == 201
@@ -45,14 +45,14 @@ def test_task_route_missing_data(client):
     del json_data[missing_attribute]
 
     # Make a POST request to the '/task' route
-    response = client.post('/task', json=json_data)
+    response = client.post(TASK_ROUTE, json=json_data)
 
     # Check the response status code and message
     assert response.status_code == 400
     assert response.json['message'] == "The {missing_attribute} attribute is missing in the input to create a task."
 
 def test_task_route_accepts_post(client):
-    response = client.post('/task')
+    response = client.post(TASK_ROUTE)
     assert response.status_code == 201
 
 def test_hello_world():
@@ -61,10 +61,14 @@ def test_hello_world():
     assert response.data == b'Hello, World!'
 
 
-def message_data_json_dummy() -> dict:
-    return {
+
+def message_data_json_dummy(**kwargs) -> dict:
+    dummy_data = {
         "user_id": "342342fsd",
         "group_ids": ["sdfds", "2131fsdf"],
         "message_data": "test data",
         "time_to_send": "yyyy-MM-dd"
     }
+    dummy_data.update(kwargs)
+
+    return dummy_data
--- views.py
+++ views.py
@@ -2,6 +2,8 @@ import schedule
 from flask import Blueprint
 from log.log_handler import log
 
+TASK_ROUTE = '/task'
+
 # Create a Blueprint object
 views_blueprint = Blueprint('views', __name__)
 
@@ -13,6 +15,13 @@ def hello():
     job_that_executes_once()
     print("2 - Hello, World!")
     return 'Hello, World!'
+
+@views_blueprint.route(TASK_ROUTE, methods=['POST'])
+@log
+def task():
+    print("1 -Hello, World!")
+    job_that_executes_once()
+    return 'Post task', 201
 
 def job_that_executes_once():
     # Do some work that only needs to happen once...
