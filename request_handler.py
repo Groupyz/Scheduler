8326b8fabbc5e943b275c4b6b0eaaec0b42f6aad
ndvisme <herespier@yahoo.com>
2023-09-02 12:01:03 +0300

    shorten url var name.

--- request_handler.py
+++ request_handler.py
@@ -5,10 +5,10 @@ from message import Message
 
 BOT_URL = os.environ.get("BOT_URL")
 
-BOT_URL_POST_MESSAGE = BOT_URL + '/sendMessage'
+BOT_URL_MSG = BOT_URL + '/sendMessage'
 
 
-def post_message(url: str = BOT_URL, message: Message = None) -> response:
+def post_message(url: str = BOT_URL_MSG, message: Message = None) -> response:
     message_as_json = message.to_json()
     res = requests.post(url, json=message_as_json, timeout=10)
 
